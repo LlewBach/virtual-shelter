45ec2d5ad64fa837fc6d46dfa5679cf8569b5d84
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-08-28 18:30:45 +0100

    Test home view

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeViewTests(TestCase):
+    """ Unit tests for Home app view """
+    def test_index_view(self):
+        """ Test home page renders correct page """
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
