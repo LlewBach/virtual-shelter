3ac51e1a40aa2e74e9155757218ed6fc446ad2f8
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-08-29 20:22:19 +0100

    Test dashboard view

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class DashboardViewTests(TestCase):
+    def test_dashboard_view_status_code(self):
+        response = self.client.get('/dashboard/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'dashboard/dashboard.html')
+        
