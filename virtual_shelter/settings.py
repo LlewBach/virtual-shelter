e033a5b69a80efd8887cf16dad799af64c1fefac
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-09-22 10:42:48 +0100

    Change SECRET_KEY and set up DEBUG

--- virtual_shelter/settings.py
+++ virtual_shelter/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@vr_dp++^0yr1&8mtm@!q3l2_y0i3ck%1)&0j()mgl@gajl@d5'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
