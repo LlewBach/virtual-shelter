932cbd3bf5f7575ba6f0afd6014c8f9402690c68
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-09-26 10:09:40 +0100

    Add STATIC_ROOT to settings

--- virtual_shelter/settings.py
+++ virtual_shelter/settings.py
@@ -153,6 +153,8 @@ STATIC_URL = '/static/'
 #     BASE_DIR / 'static',
 # ]
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
