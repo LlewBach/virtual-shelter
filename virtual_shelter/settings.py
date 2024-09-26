f0199a1ba4724f4ddcdef6f9842d51daa3c0bb10
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-09-26 12:17:49 +0100

    Fix settings

--- virtual_shelter/settings.py
+++ virtual_shelter/settings.py
@@ -176,6 +176,12 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
+    STORAGES = {
+        "default": {
+            "BACKEND": "custom_storages.MediaStorage",
+        },
+        "staticfiles": {"BACKEND": "custom_storages.StaticStorage"} }
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
