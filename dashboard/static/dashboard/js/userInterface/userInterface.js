91fc1a191eb5c849d6a3bf2376dbb9e0ac794b81
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-10-08 13:48:14 +0100

    Change sprite canvas UI colour

--- dashboard/static/dashboard/js/userInterface/userInterface.js
+++ dashboard/static/dashboard/js/userInterface/userInterface.js
@@ -4,6 +4,7 @@ export class UserInterface {
   }
   draw(context) {
     // Satiation Bar
+    context.fillStyle = '#d8d9f7';
     context.fillText('Satiation: ' + this.game.satiation, 5, 15);
     context.fillRect(65, 8, this.game.satiation, 6);
   }
