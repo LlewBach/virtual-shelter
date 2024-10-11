33626c237310b990f05dde5c408c83c1d42458af
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-10-11 12:53:35 +0100

    Make species input select

--- animals/models.py
+++ animals/models.py
@@ -4,11 +4,14 @@ from profiles.models import Profile
 
 
 class Animal(models.Model):
+    class SpeciesChoices(models.TextChoices):
+        DOG = 'Dog', 'Dog'
+
     shelter = models.ForeignKey(Shelter, on_delete=models.CASCADE, related_name='animals')
     fosterer = models.ForeignKey(Profile, on_delete=models.SET_NULL, related_name='animals', blank=True, null=True)
     image = models.ImageField(upload_to='', blank=True, null=True)
     name = models.CharField(max_length=255)
-    species = models.CharField(max_length=100)
+    species = models.CharField(max_length=25, choices=SpeciesChoices.choices, default=SpeciesChoices.DOG)
     breed = models.CharField(max_length=100, blank=True, null=True)
     age = models.IntegerField()
     description = models.TextField(blank=True, null=True)
