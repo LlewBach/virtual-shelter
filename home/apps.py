7715e90f633d6a642972d8c32efa7ae4ab8a6bee
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-10-12 15:09:45 +0100

    Add docstrings to home app

--- home/apps.py
+++ home/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """
+    Configuration class for the 'home' app. Sets the default primary key field type
+    and specifies the app's name as 'home'.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
--- home/views.py
+++ home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
+    """
+    View for rendering the home page (index). Renders the 'home/index.html' template.
+    """
     return render(request, 'home/index.html')
