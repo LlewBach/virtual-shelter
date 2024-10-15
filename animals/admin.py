27303bcb396ca2ef3bd74393d1da58b70ea4052a
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-10-15 22:02:02 +0100

    Lint python

--- animals/admin.py
+++ animals/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 from .models import Animal
 
+
 class AnimalAdmin(admin.ModelAdmin):
     """
     Admin configuration for the Animal model.
-    
-    Displays the shelter, image, name, species, breed, age, and adoption status 
+
+    Displays the shelter, image, name, species, breed, age, and adoption status
     in the list display of the admin interface.
     """
-    list_display = ('shelter', 'image', 'name', 'species', 'breed', 'age', 'adoption_status')
+    list_display = (
+        'shelter',
+        'image',
+        'name',
+        'species',
+        'breed',
+        'age',
+        'adoption_status'
+        )
+
 
 admin.site.register(Animal)
